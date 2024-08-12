d1eb49a719475417b09bb53148b11370b5439a05
kavrazbusra <168726400+kavrazbusra@users.noreply.github.com>
2024-08-12 17:29:22 +0300

    eğitim 1 / eğitim 2

---
+++ eğitim 1.py
@@ -0,0 +1,8 @@
+x = 4 + 5
+'''değişkenime sayısal işlemler yaptım'''
+y = 'Doğukanın 9 tane dersi var ' + 'Büşranın 4 tane dersi var'
+'''değişkenime iki cümleyi birleştirme yaptım'''
+print(x)
+'''ekrana x çıktısını verdi'''
+print(y)
+'''ekrana y çıktısını verdi'''
---
+++ eğitim 2.py
@@ -0,0 +1,54 @@
+def mytype():
+    print(4)
+print('Doğukan birazdan bana fonksiyonu gösterecek')
+mytype()
+def myvar(busra):
+    print('Büşranın kaldığı ders sayısı=' , busra)
+myvar(1)
+
+'''
+1.fonksiyonum toplama
+2.fonksiyonum çıkartma
+3.fonksiyonum çarpma
+4.fonksiyonum bölme
+her fonksiyonun dışardan gelen 2 tane değişkeni olacak
+fonksiyonlarımın adı altında işlemler yapılıp ekrana işlemler verilecek
+
+'''
+
+def myplus(a , b):
+    print('a değeri ile b değeri=' , a+b)
+'''
+fonksiyonu oluşturup, değişken tanımladım. Bu değişkenlerime toplama işlemi yaptım.
+'''
+
+def mycikartma(a , b):
+    print('a değeri ile b değeri' , a-b)
+'''
+fonksiyonu oluşturup, değişken tanımladım. Bu değişkenlerime çıkarma işlemi yaptım.
+'''
+
+
+def mycarpma(a , b):
+    print('a değeri ile b değeri' , a*b)
+'''
+fonksiyonu oluşturup, değişken tanımladım. Bu değişkenlerime çarpma işlemi yaptım.
+'''
+
+
+def mydivide(a , b):
+    print('a değeri ile b değeri' , a/b)
+'''
+fonksiyonu oluşturup, değişken tanımladım. Bu değişkenlerime bölme işlemi yaptım.
+'''
+
+myplus(2,3)
+mycikartma(2,3)
+mycarpma(2,3)
+mydivide(2,3)
+'''
+fonksiyonu çağırdım ve değişkenlerime değer atadım.
+'''
+
+
+
